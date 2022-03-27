93202634a343c3c557b46a4661c5625fa8589e23
Rithika Sapparapu <sapparapurithika@gmail.com>
2022-03-27 18:20:19 +0530

    update template-production

--- code-clone-analysis/template-production/step1.cpp
+++ code-clone-analysis/template-production/step1.cpp
@@ -1,3 +1,44 @@
 #include <bits/stdc++.h>
+#include <regex>
 using namespace std;
 
+vector<string> datatypes = {"char","signed char","unsigned char","short","signed short","signed short int","unsigned short", "unsigned short int", "int", "signed int", "unsigned int", "long","signed long","signed long int", "unsigned long", "unsigned long int", "long long","long long int","signed long long","signed long long int","unsigned long long","unsigned long long int","float", "double", "long double"};
+vector<string> symbols = {"(",")","{","}","[","]",";","=","+","-","/","%"};
+bool belongsTo(string str, vector<string> container){
+    for(int i=0; i<datatypes.size(); i++){
+        if(str == datatypes[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
+//it could be 10, 13.4, 'a', "hello world"
+bool isAconst(string str){
+
+}
+
+unordered_map<string, vector<pair<int, vector<string>>>> returnTemplateCode(unordered_map<string, vector<pair<int, vector<string>>>> module){
+    unordered_map<string, vector<pair<int, vector<string>>>> templatecode;
+    for(auto item: module){
+        //item.first
+        vector<pair<int, vector<string>>> temp;
+        for (int i = 0; i < item.second.size(); i++){
+            temp[i].first = item.second[i].first;
+            //temp[i].second is the vector<string> part
+
+            for(int j=0; j<item.second[i].second.size(); j++){
+                if(belongsTo(item.second[i].second[j],datatypes)){
+                    temp[i].second.push_back("DATA");
+                }
+                else if(belongsTo(item.second[i].second[j],symbols)){
+                    temp[i].second.push_back(item.second[i].second[j]);
+                }
+                else if(isAconst(item.second[i].second[j])){
+                    
+                }
+            }
+        }
+        templatecode[item.first] = temp;
+    }
+}
